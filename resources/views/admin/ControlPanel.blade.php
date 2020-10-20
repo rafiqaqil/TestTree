90e1c148c9a28daca0ca80e5b18637498f9cc583
ASIDS <59718093+ASIDS@users.noreply.github.com>
2020-10-21 03:07:52 +0800

    Update ControlPanel.blade.php

--- resources/views/admin/ControlPanel.blade.php
+++ resources/views/admin/ControlPanel.blade.php
@@ -11,27 +11,25 @@
                                         <h4>Midnight Updates (Force Action)</h4>
                                         <p>DM5 Will automatically input new reentry nodes and DM3 if full with 5 Dexcendants</p>
             <hr>
-            <a href="{{env('absolute')}}/MDC/Update/DM3"><button class="btn btn-warning">Update DM3 </button></a>
-            <a href="{{env('absolute')}}/MDC/Update/DM5"><button class="btn btn-warning">Update DM5 </button></a>
-              <a href="{{env('absolute')}}/MDC/Update/Sponsor"><button class="btn btn-warning">Update Sponsor </button></a>
+            <a href="{{env('absolute')}}/MDC/Update/DM3"><button class="btn btn-warning"> DM3 </button></a>
+            <a href="{{env('absolute')}}/MDC/Update/DM5"><button class="btn btn-warning"> DM5 </button></a>
+              <a href="{{env('absolute')}}/MDC/Update/Sponsor"><button class="btn btn-warning"> Sponsor </button></a>
               <br>
                       
             <hr>  <h4>Master Tree Views</h4>
-             <li class="nav-item">
+            
             <a class="nav-link" href="{{env('absolute')}}/DM5-G">
               <i class="fas fa-fw fa-table"></i>
               <span>DM-5 Master Tree</span></a>
-          </li>
-             <li class="nav-item">
+        
             <a class="nav-link" href="{{env('absolute')}}/DM3-G">
               <i class="fas fa-fw fa-table"></i>
               <span>DM-3 Master Tree</span></a>
-          </li>
-           <li class="nav-item">
+         
             <a class="nav-link" href="{{env('absolute')}}/sponsor-G">
               <i class="fas fa-fw fa-table"></i>
               <span>Sponsor Master Tree</span></a>
-          </li>
+        
             <hr>
 
             
