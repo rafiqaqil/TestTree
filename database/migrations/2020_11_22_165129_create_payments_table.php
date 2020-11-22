0afe9e3679feb44e56c11f64796007095d85ef45
ASIDS <59718093+ASIDS@users.noreply.github.com>
2020-11-23 01:25:54 +0800

    Update 2020_11_22_165129_create_payments_table.php

--- database/migrations/2020_11_22_165129_create_payments_table.php
+++ database/migrations/2020_11_22_165129_create_payments_table.php
@@ -16,6 +16,25 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id'); // OWNER OF REQUEST
+            $table->decimal('AMOUNT', 8, 2);
+            $table->string('STATUS')->default('NEW'); // 0 NEW -1 CANCEL 1 APPROVED
+            $table->string('WAY'); // IN OR OUT
+            
+            $table->string('DETAIL'); // IN OR OUT
+            $table->integer('INFO_INT')->default(-1);// IN OR OUT
+             $table->string('INFO_STR')->default('NONE');// IN OR OUT
+            
+            
+            
+            $table->integer('RESERVE_INT1')->default(0);
+            $table->decimal('RESERVE_DEC1', 8, 2)->default(0.0);
+            $table->string('RESERVE_STR1')->default('UNDEFINED');
+             
+             $table->integer('RESERVE_INT2')->default(0);
+             $table->decimal('RESERVE_DEC2', 8, 2)->default(0.0);
+             $table->string('RESERVE_STR2')->default('UNDEFINED');
+            
         });
     }
 
