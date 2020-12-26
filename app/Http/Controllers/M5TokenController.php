4c7d5da6266821c15c20f082a4a502ebbc31206d
Amirul Sadikin <37245308+aiamirul@users.noreply.github.com>
2020-12-26 14:28:39 +0800

    fIX m5 Token calulcationTimes 10 Mebershitype p

--- app/Http/Controllers/M5TokenController.php
+++ app/Http/Controllers/M5TokenController.php
@@ -9,7 +9,7 @@ class M5TokenController extends Controller
        public function index(){
                
                 $user = auth()->user();   $profile = $user->profile()->first();
-                $FinalBalance = $profile->membership_type;
+                $FinalBalance = $profile->membership_type*10;
                  return view('m5token.index',compact('FinalBalance','profile'));
            }
 }
